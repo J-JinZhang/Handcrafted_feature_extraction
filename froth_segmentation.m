3dc7b8a4dde1fb37343ecfccb255ae2f56705cfa
J. Zhang <j.zhang.vision@gmail.com>
2022-02-20 01:14:48 +0800

    Update froth_segmentation.m

--- froth_segmentation.m
+++ froth_segmentation.m
@@ -1,15 +1,21 @@
-%%
+"""
+Author: Dr. Jin Zhang 
+E-mail: j.zhang.vision@gmail.com
+Created on 2022.02.20
+"""
+
+
 clc;
 clear all
 close all
 
 
 %%
-filename = 'L:/FrothVideo/201791/20170901000039.avi'; 
+filename = 'L:/FrothVideo/20160601000012.avi'; 
 Obj = VideoReader(filename);
 nFrames = Obj.NumberOfFrames;
 vidHeight = Obj.Height;     vidWidth = Obj.Width;
-step = 15*2; %有15个重复帧
+step = 15*2; 
 % start = 1+step0*13;     ending = nFrames-step0*0;        %test
 % start = nFrames-600-step0*0;     ending = nFrames;      %test
 start = 1+step*6;     ending = nFrames;
@@ -19,7 +25,7 @@ feature = [];
 for k = start:step:ending
     im = read(Obj, k);
     close all
-    % im = imread('D:/泡沫图像分割样本/锌/3.BMP');
+    
     I = im2double(rgb2gray(im));
     temp = mapminmax(I(:)', 0, 1);
     I = reshape(temp, size(I));
@@ -29,18 +35,15 @@ for k = start:step:ending
     I = imfilter(imadjust(I), filter);
     figure, imshow(I)
         
-    %%  图像增强  %%
-    % 开重构：先腐蚀再重构
     se = strel('disk', 3);
     Ioc = imopen(I, se);
     Iobr = imreconstruct(Ioc, I);
-    % 对开重构后的图像进行闭重构：先膨胀再重构
+    
     Iobrd = imclose(Iobr, se);
     % Iobrcbr = imreconstruct(Iobr, Iobrd);
     Iobrcbr = imreconstruct(Iobrd, Iobr);
-    % figure, imshow(Iobrcbr),    title('Iobrcbr image');     %%%% 开闭滤波图像
+    % figure, imshow(Iobrcbr),    title('Iobrcbr image');
 
-    %%  标签引导下的分水岭分割  %%
     Iec_minimal = imadjust(imcomplement(Iobrcbr));
     figure, imshow(Iec_minimal, []);
     flag_minimal = imextendedmin(Iec_minimal,30/255);   
